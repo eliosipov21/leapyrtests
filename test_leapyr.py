0e59d12545f3b39df5ba373f324928cd526eed2c
eliosipov21 <eli.osipov2@gmail.com>
2021-05-22 19:16:53 -0700

    Fixed test

--- test_leapyr.py
+++ test_leapyr.py
@@ -8,6 +8,16 @@ class testCaseAdd(unittest.TestCase):
     def test_leap2(self):
         result2 = leapyr.leapyear(2018)
         self.assertEqual(result2, False)
+    def test_leap3(self):
+        result3 = leapyr.leapyear(99999999)
+        self.assertEqual(result3, False)
+    def test_leap4(self):
+        result4 = leapyr.leapyear(0)
+        self.assertEqual(result4, False)
+    def test_leap5(self):
+        result5 = leapyr.leapyear(-6)
+        self.assertEqual(result5, False)
+    
 
 if __name__ == '__main__':
     unittest.main()
